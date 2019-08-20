1a1e62816beb51e1d559005b4684ad277b9269ab
Katherin <katherin@DESKTOP-AQNLT1P.localdomain>
2019-08-20 16:00:23 +0100

    background-img

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar/Navbar.js';
+import background from './backround-img.jpg';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <Navbar></Navbar>    
+      <img className='background-img' src={background} alt='tecnology picture' />
+        <Navbar></Navbar>  
       </header>
+      <div>
+      <p>Hi, I'm Katherin. </p>
     </div>
+    <div>
+      <p>Want to know more about me?</p>
+    </div>
+  </div>
   );
 }
 
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -4,13 +4,10 @@ import './Navbar.css';
 export default function Navbar() {
     return (
         <div className="Navbar">
-            <div className="MFlix">
-                <h2>About Me</h2>
-            </div>
-            <div className="Login">
-                <h4>Login</h4>
-                <h4>Sign up</h4>
-            </div>
+            <div className='logo'>
+                <p>Katherin</p>
+            </div>   
         </div>
+        
     );
 }
