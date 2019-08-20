903ee175a171f95956403d7f4632778d24924dfb
Katherin <katherin@DESKTOP-AQNLT1P.localdomain>
2019-08-20 11:39:30 +0100

    Updates

---
+++ precache-manifest.8bbf99cc7f8d49d8a375f489a98f43ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de11a2f08f9e71207f0549bc2770c6f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "b2eeb96500344b842934",
+    "url": "/Portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f80f0acbd2d0ce47e57c",
+    "url": "/Portfolio/static/js/2.917ae229.chunk.js"
+  },
+  {
+    "revision": "b2eeb96500344b842934",
+    "url": "/Portfolio/static/js/main.63dc7e48.chunk.js"
+  },
+  {
+    "revision": "9534e064c61335e7c934",
+    "url": "/Portfolio/static/js/runtime~main.3a3ea873.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
